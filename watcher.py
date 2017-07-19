4247f841b80f3525cef96082a91fbb82dc20c209
Bob the Hamster <Bob@HamsterRepublic.com>
2017-07-19 11:24:49 -0700

    Add a -c command-line argument to just print the current desktop id number (also some other command-line argument cleanup)

--- watcher.py
+++ watcher.py
@@ -44,8 +44,8 @@ class Watcher(object):
 
 def is_installed(progname):
     cmd = ["which", progname]
-    if subprocess.call(cmd) == 0:
-        print "  Found installed OK!"
+    if subprocess.call(cmd, stdout=subprocess.DEVNULL) == 0:
+        print "{0} found installed OK!".format(progname)
         return True
     print "{0} does not seem to be installed yet.".format(progname)
     return False
@@ -69,9 +69,19 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("interval", type=float)
-    parser.add_argument("screens", type=int, nargs="+", metavar="screen")
+    parser.add_argument("interval", nargs="?", default=3.0, type=float, help="The number of seconds to wait between each screenshot, for example, 3.0")
+    parser.add_argument("screens", type=int, nargs="*", metavar="desktop", help="The id numbers of the virtual desktops you want to timelapse. For example, 0 would only timelapse the first desktop. 0 1 3 would timelapse the first, second, and fourth virtual desktop, while ignoring the third")
+    parser.add_argument("-c", "--current", action="store_true", help="Just print the ID number of the current desktop and exit.")
     args = parser.parse_args()
+    
+    if args.current:
+        print get_curdesk()
+        sys.exit(0)
+    
+    if len(args.screens) == 0:
+        print "You must specify at least one virtual desktop ID number."
+        parser.print_help()
+        sys.exit(1)
 
     app = Watcher(interval=args.interval, screens=args.screens)
     print get_curdesk()
